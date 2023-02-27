781738facfc2c7d04505cb86f78eebfb00faa299
Danilo Portela <daniloportela97@gmail.com>
2023-02-27 11:48:51 -0300

    Estrutura inicial do projeto.

---
+++ lib/screens/place_form_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class PlaceFormScreen extends StatefulWidget {
+  const PlaceFormScreen({Key? key}) : super(key: key);
+
+  @override
+  State<PlaceFormScreen> createState() => _PlaceFormScreenState();
+}
+
+class _PlaceFormScreenState extends State<PlaceFormScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Novo Lugar'),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text('Form!!!'),
+      ),
+    );
+  }
+}
---
+++ lib/screens/places_list_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:great_places/utils/app_routes.dart';
+
+class PlacesListScreen extends StatelessWidget {
+  const PlacesListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Meus Lugares"),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).pushNamed(AppRoutes.placeForm);
+            },
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: const Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}

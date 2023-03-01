94df419ab961940558163f7a4d75ca095d33533e
Danilo Portela <daniloportela97@gmail.com>
2023-03-01 14:31:59 -0300

    Usando SQLite#02.

--- lib/utils/db_util.dart
+++ lib/utils/db_util.dart
@@ -23,4 +23,9 @@ class DbUtil {
       conflictAlgorithm: sql.ConflictAlgorithm.replace,
     );
   }
+
+  static Future<List<Map<String, dynamic>>> getData(String table) async {
+    final db = await DbUtil.database();
+    return db.query(table);
+  }
 }
